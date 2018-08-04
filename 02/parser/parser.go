74105efcf8525e42716b48d89e3962d0744d668f
dayoshidayoshi00444 <girubado00444@gmail.com>
2018-08-04 23:45:58 +0900

    update

--- 02/parser/parser.go
+++ 02/parser/parser.go
@@ -2,4 +2,32 @@ package parser
 
 import (
 	"github.com/dayoshidayoshi00444/Go_interpreter/02/ast"
+	"github.com/dayoshidayoshi00444/Go_interpreter/02/lexer"
+	"github.com/dayoshidayoshi00444/Go_interpreter/02/token"
 )
+
+type Parser struct {
+	l *lexer.Lexer
+
+	curToken  token.Token
+	peekToken token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+
+	// 2つトークンを読み込む．curTokenとpeekTokenの両方がセットされる
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParserProgram() *ast.Program {
+	return nil
+}
