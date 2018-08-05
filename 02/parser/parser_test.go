9859ab483be6d7f0b254aafab01b3226215d3899
dayoshidayoshi00444 <girubado00444@gmail.com>
2018-08-06 00:31:46 +0900

    39ページまで

---
+++ 02/parser/parser_test.go
@@ -0,0 +1,64 @@
+package parser
+
+import (
+	"github.com/dayoshidayoshi00444/Go_interpreter/02/ast"
+	"github.com/dayoshidayoshi00444/Go_interpreter/02/lexer"
+	"testing"
+)
+
+func TestLetStatements(t *testing.T) {
+	input := `
+	let x = 5;
+	let y = 10;
+	let foobar = 838383;
+	`
+	l := lexer.New(input)
+	p := New()
+
+	program := p.ParseProgram()
+	if program == nil {
+		t.Faralf("program.Statements does not contain 3 statements. got=%d",
+			len(progmran.Statements))
+	}
+
+	tests := []struct {
+		expecteIndentifier string
+	}{
+		{"x"},
+		{"y"},
+		{"foobar"},
+	}
+
+	for i, tt := range tests {
+		stmt := program.Statements[i]
+		if !testLetStatement(t, stmt, tt.expecteIndentifier) {
+			return
+		}
+	}
+}
+
+func testLetStatement(t *testing.T, s ast.Statment, name string) bool {
+	if s.TokenLiteral() != "let" {
+		t.Errorf("s.TokenLiteral not 'let'. got=%q", s.TokenLiteral())
+		return false
+	}
+
+	letStmt, ok := s.(*ast.LetStatement)
+	if !ok {
+		t.Errorf("s not *ast.LetStatement. got=%T", s)
+		return false
+	}
+
+	if letStmt.Name.Value != name {
+		t.Errorf("letStmt.Name.Value not '%s'. got=%s", name, letStmt.Name.Value)
+		return false
+	}
+
+	if letStmt.Name.TokenLiteral() != name {
+		t.Errorf("letStmt.Name.TokenLiteral() not '%s'. got=%s",
+			name, letStmt.Name.TokenLiteral())
+		return false
+	}
+
+	return true
+}
